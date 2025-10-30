704beca86971482b098017660f2e08cb414039d8
Rida-Gony <missriicreate12@gmail.com>
2025-10-30 14:54:03 +0700

    Fix navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../../assets/mainlogo.jpg";
+import Logo from "../../assets/mainLogo.jpg";
 import { IoMdSearch } from "react-icons/io";
 import { FaCartShopping } from "react-icons/fa6";
 import { FaCaretDown } from "react-icons/fa";
